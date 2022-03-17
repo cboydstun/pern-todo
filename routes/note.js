4e6d75a42ae682953fb8c83908b712682bb4b7a8
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2022-03-17 03:20:42 -0500

    route controllers

--- routes/note.js
+++ routes/note.js
@@ -1,27 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const {
+  getNotes,
+  createNotes,
+  updateNotes,
+  deleteNotes,
+} = require('../controllers/noteController');
 
-//gets all notes
-router.get("/", (req, res) => {
-  res.send("gets all notes");
-});
-
-//create a note
-router.post("/", (req, res) => {
-    console.log(req.body);
-    res.send("creates a note");
-});
-
-//edit a note
-router.put("/:id", (req, res) => {
-    console.log(req.body);
-    res.send("edits a note");
-});
-
-//delete a note
-router.delete("/:id", (req, res) => {
-    console.log(req.body);
-    res.send("deletes a note");
-});
+router.route('/').get(getNotes).post(createNotes);
+router.route('/:id').put(updateNotes).delete(deleteNotes);
 
 module.exports = router;
